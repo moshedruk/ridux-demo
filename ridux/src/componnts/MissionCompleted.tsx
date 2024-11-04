3d58b3bde5fce645035365840f7cc83dd7a50a0a
moshedruk <dmd6708713@gmail.com>
2024-11-04 17:48:17 +0200

    fix row

--- ridux/src/componnts/MissionCompleted.tsx
+++ ridux/src/componnts/MissionCompleted.tsx
@@ -6,18 +6,12 @@ export default function MissionCompleted() {
     const deploymentContext = useContext(DeploymentContext);
     if (!deploymentContext) {
         throw new Error("MissionCompleted must be used within a DeploymentProvider");
-        }
-        const { units } = deploymentContext;    
-        
-       
-        const allComplete = Object.values(units.units).every(status => status === 'complete');
-            
-        
-  return (
-    <div>
-        
-      {allComplete && <h1>all units complete </h1>}
-      
-    </div>
-  )
+    }
+    const { units } = deploymentContext;
+    const allComplete = Object.values(units.units).every(status => status === 'complete');
+    return (
+        <div>
+            {allComplete && <h1>all units complete </h1>}
+        </div>
+    )
 }
